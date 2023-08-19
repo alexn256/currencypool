06fdcf6966c411ec60f49ec8b6fd1255df648e34
Alexander Naumov <naumov.btm@gmail.com>
2023-08-19 20:06:38 +0300

    tmp

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,11 +1,61 @@
-import {Column} from "./Column";
+
+import {Bar} from "react-chartjs-2";
+import { Chart, registerables} from 'chart.js';
+
+Chart.register(...registerables);
 
 
 export const Board = ({rates}) => {
-    const values =  getBoardData(rates);
+    const values = getBoardData(rates);
+
+
+    const labels = values.map((item, index) => index);
+
+    const options = {
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart',
+            },
+        },
+        scales: {
+            yAxes: [{
+                id: 'A',
+                type: 'linear',
+                position: 'left',
+            }, {
+                id: 'B',
+                type: 'linear',
+                position: 'right',
+                ticks: {
+                    max: 1,
+                    min: 0
+                }
+            }]
+        }
+    };
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: values.map((item, index) => item.rate),
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            }
+        ],
+    };
+
     return (
         <div className="row-wrapper">
-            <div className="board">{values.map(data => <Column key={data.id} columnData={data}/>)}</div>
+            <div className="board">
+                <Bar options={options} data={data} />
+            </div>
         </div>
     );
 }
